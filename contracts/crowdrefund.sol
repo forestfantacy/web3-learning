2131c1f6c0f20352c05703bd6c9a80d0d37fbcb2
forestfantacy <13717507049@163.com>
2023-01-03 16:46:30 +0800

    123

--- contracts/crowdrefund.sol
+++ contracts/crowdrefund.sol
@@ -2,6 +2,10 @@
 
 pragma solidity >=0.5.0 <0.9.0;
 
+/**
+多人筹款
+对款项花销投票，票数过半，管理员可以执行下单
+*/
 contract CrowdRefund {
     mapping(address => uint256) public contributors;
     uint256 public noOfContributors;
@@ -14,6 +18,18 @@ contract CrowdRefund {
     uint256 public raisedAmount;
 
     address public admin;
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint256 value;
+        bool completed;
+        uint noOfVotes;
+        mapping(address => bool) voters;
+    }
+
+    mapping(uint256 => Request) public requests;
+    uint256 public numRequests;
 
     constructor(uint256 _goal, uint256 _deadline) {
         goal = _goal;
@@ -44,5 +60,51 @@ contract CrowdRefund {
     // 查询已筹款金额
     function getBalance() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function refund() public payable {
+        // require(block.timestamp > endTime);
+        require(raisedAmount <= goal);
+        require(contributors[msg.sender] > 0, "has money");
+
+        uint256 money = contributors[msg.sender];
+        contributors[msg.sender] = 0;
+        raisedAmount -= money;
+
+        address payable recipient = payable(msg.sender);
+        recipient.transfer(money);
+    }
+
+    function createRequest(string memory _description,address payable _recipient,uint _value) public {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+
+        newRequest.completed = false;
+        newRequest.noOfVotes = 0;
+    }
+
+    // 捐款者身份,为指定提案投票
+    function voteRequest(uint _requestNo) public{
+        require(contributors[msg.sender] > 0);
+        Request storage thisRequest = requests[_requestNo];
+
+        require(thisRequest.voters[msg.sender] == false);
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.noOfVotes ++;
+    }
+
+    function makePayment(uint _requestNo) public {
+        require(admin == msg.sender);
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.completed == false);
+        require(thisRequest.noOfVotes > noOfContributors / 2);
+
+        thisRequest.recipient.transfer(thisRequest.value);
+        thisRequest.completed = true;
+
     }
 }
